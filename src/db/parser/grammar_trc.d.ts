3e7a7c2bd299c87aa6823d314710b292bc769e94
KPMGE <jesuskevin254@gmail.com>
2024-06-18 23:31:52 -0300

    :recycle: remove unneeded comments

--- src/db/parser/grammar_trc.d.ts
+++ src/db/parser/grammar_trc.d.ts
@@ -1,4 +1,3 @@
-
 declare namespace trcAst {
 	type Operator = '=' | '!=' | '<' | '>' | '<=' | '>='
 	type Quantifier = 'exists' | 'forAll'
@@ -48,54 +47,3 @@ declare namespace trcAst {
 		formula: LogicalExpression
 	}
 }
-
-/*
-declare namespace trcAst {
-	type astNode = rootTrc
-
-	interface rootTrc {
-		type: 'trcRoot',
-		child: Expression,
-		child2: undefined,
-	}
-
-	interface Expression {
-		type: 'expression',
-		projection: Projection,
-		predicate: Predicate
-	}
-
-	interface Projection {
-		type: 'projection',
-		relation: string,
-		columns: string[]
-	}
-
-	interface Predicate {
-		type: 'predicate',
-		quantifier: string,
-		relation: string,
-		relationAlias: string,
-		condition: booleanExpr
-	}
-
-	// interface Condition {
-	// 	type: 'comparison',
-	// 	attribute: string,
-	// 	operator: string,
-	// 	value: number | string
-	// }
-
-	interface valueExpr {
-		type: 'valueExpr',
-		child: undefined,
-		child2: undefined,
-		assignments: undefined,
-		datatype: 'string' | 'boolean' | 'number' | 'null' | 'date',
-		func: relalgAst.ValueExprFunction,
-		args: valueExpr[] | any[],
-	}
-
-	interface booleanExpr extends valueExpr { }
-}
-*/
