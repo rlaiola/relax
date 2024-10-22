2be44e04e47d74cb25400d3a312b87a4c9cad5b9
Rodrigo Laiola Guimarães <rlaiola@users.noreply.github.com>
2024-10-22 11:48:14 -0300

    Update relalgFromAst.ts

--- src/db/translate/relalgFromAst.ts
+++ src/db/translate/relalgFromAst.ts
@@ -538,13 +538,32 @@ export function relalgFromRelalgAstNode(astNode: relalgAst.relalgOperation, rela
 						const el = n.arg[i];
 
 						if (el.type === 'column' &&
-							el.name === '*' &&
-								el.relAlias === null) {
+						    el.name === '*') {
+							if (el.relAlias === null) {
+								// project all columns
+								let cols;
+								try {
+									cols = child.getSchema();
+								}
+								catch (e) {
+									cols = null;
+								}
+								if (cols) {
+									for (let i = 0; i < cols.getSize(); i++) {
+										// normal columns
+										projections.push(cols.getColumn(i));
+									}
+								}
+								else // normal columns
+									projections.push(new Column(el.name, el.relAlias));	
+							}
 							// project all columns
-							let cols = child.getSchema();
-							for (let i = 0; i < cols.getSize(); i++) {
-								// normal columns
-								projections.push(cols.getColumn(i));
+							else if (child.getMetaData('fromVariable') &&
+											 child.getMetaData('fromVariable') === el.relAlias) {
+								projections.push(new Column(el.name, null));	
+							}
+							else {
+								projections.push(new Column(el.name, el.relAlias));	
 							}
 						}
 						else if (el.type === 'columnName') {
